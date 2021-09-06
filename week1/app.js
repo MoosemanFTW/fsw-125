ea00eab3fed4283b06edd7c2b50d8a3e822924b8
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2021-09-05 21:06:22 -0700

    save point

---
+++ week1/app.js
@@ -0,0 +1,6 @@
+const math = require('./math')
+
+console.log(math.addition(10,2))
+console.log(math.subtraction(20,16))
+console.log(math.division(100,5))
+console.log(math.multiplacation(50,4))
---
+++ week1/math.js
@@ -0,0 +1,25 @@
+const sum = (a,b) =>{
+    return a + b
+};
+
+
+const sub = (a,b) =>{
+    return a - b
+};
+
+
+const div = (a,b) =>{
+    return a / b
+};
+
+
+const multi = (a,b) =>{
+    return a * b
+};
+
+module.exports = {
+    addition: sum,
+    subtraction: sub,
+    division: div,
+    multiplacation: multi
+}
